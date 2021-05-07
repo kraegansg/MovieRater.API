aa421548272526c5e1b1e3a71cd312084b789a1d
Charles Lucas <charlesdlucas1975@gmail.com>
2021-05-07 12:50:33 -0400

    Add Services Charles

---
+++ MovieRater.Service/FilmService.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRater.Service
+{
+   public class FilmService
+    {
+        public bool CreateFilms (FilmCreate model)
+        {
+            var entity =
+                new Film()
+                {
+                    FilmId = model.FilmId,
+                    Title = model.Title,
+                    Genre = model.Genre,
+                    FilmRating = model.FilmRating,
+                    RatingStars = model.RatingStars,
+                    //ReleaseDate = model.ReleaseDate
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Films.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<FilmListItem> GetFilms()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Films
+                    .Select(
+                        e =>
+                        new FilmListItem
+                        {
+                            FilmId = e.FilmId,
+                            Title = e.Title,
+                            RatingStars = e.RatingStars
+                        });
+                return query.ToArray();
+            }
+        }
+
+        public FilmDetail GetFilmById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                   ctx
+                   .Films
+                   .Single(e => e.FilmId == id);
+                return
+                    new FilmDetail
+                    {
+                        FilmId = entity.FilmId,
+                        Title = entity.Title,
+                        RatingStars = entity.RatingStars
+                    };
+            }
+        }
+
+        public bool UpdateFilm (FilmEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Films
+                    .Single(e => e.FilmId == id);
+
+                entity.Title = model.Title;
+                entity.RatingStars = model.RatingStars;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeleteFilm(int FilmId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                   ctx
+                   .Films
+                   .Single(e => e.FilmId == filmId);//?
+
+                ctx.TVs.Remove(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
---
+++ MovieRater.Service/TVService.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRater.Service
+{
+   public class TVService
+    {
+        public bool CreateTV(TVCreate model)
+        {
+            var entity =
+                new TV()
+                {
+                    Title = model.Title,
+                    Channel = model.Channel,
+                    Genre = model.Genre,
+                    //TVRating = model.TVRating,
+                    //RatingStars = model.RatingStars
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.TVs.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<TVListItem> GetTVs()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .TVs
+                    .Select(
+                        e =>
+                        new TVListItem
+                        {
+                            TVId = e.TVId,
+                            Title = e.Title,
+                            RatingStars = e.RatingStars
+                        });
+                return query.ToArray();
+            }
+        }
+
+        public TVDetail GetTVById (int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .TVs
+                    .Single(e => e.TVId == id);
+                return
+                    new TVDetail
+                    {
+                        TVId = entity.TVId,
+                        Title = entity.Title,
+                        RatingStars = entity.RatingStars
+                    };
+            }
+        }
+
+        public bool UpdateTV(TVEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .TVs
+                    .Single(e => e.TVId == model.TVId);
+
+                entity.Title = model.Title;
+                //entity.RatingStars = model.RatingStars;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeleteTV(int TVId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .TVs
+                    .Single(e => e.TVId == tvId);
+
+                ctx.TVs.Remove(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
